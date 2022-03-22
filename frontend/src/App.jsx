e86dead1866f30a759fd427f03dd5dc9fb50e48e
Vinesh Kannan <vingkan@gmail.com>
2022-03-22 22:24:04 +0000

    Add basic styles and show backend status.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 function App() {
+  const [hello, setHello] = useState({})
+
   useEffect(async () => {
     const req = await fetch(BACKEND_URL)
     const res = await req.json()
-    console.log(res)
+    setHello(res)
   }, [])
 
   return (
     <div className="App">
       <h1>RunFlex</h1>
+      <p>An app for runners to track and share their achievements.</p>
+      <h2>Status</h2>
+      <p>What is the status of the backend?</p>
+      <p>Let us check:</p>
+      <pre>{JSON.stringify(hello, null, 2)}</pre>
     </div>
   )
 }
