7d87702aa9fd975b56142c60f3aa97f237a63218
Vinesh Kannan <vingkan@gmail.com>
2022-03-23 06:27:33 +0000

    Load data onGitPod startup.

--- frontend/src/pages/StatusPage.jsx
+++ frontend/src/pages/StatusPage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useBackendFetchJson } from '../common/utils';
 
 export default function StatusPage() {
-  const status = useBackendFetchJson({ route: '/' })
+  const status = useBackendFetchJson({ route: '/status' })
 
   return (
     <div className="StatusPage MainPanel Page">
